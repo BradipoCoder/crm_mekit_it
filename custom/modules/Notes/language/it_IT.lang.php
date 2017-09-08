58c381d6ca2ba3dd4d3cf59711fba901621c594a
jakabadambalazs <jakabadambalazs@gmail.com>
2017-09-08 16:53:24 +0200

    fixes for notes subpanel in tasks

--- custom/modules/Notes/language/it_IT.lang.php
+++ custom/modules/Notes/language/it_IT.lang.php
@@ -3,5 +3,8 @@
 $mod_strings = array (
   'LBL_IMAGE' => 'Immagine',
   'LBL_CASES' => 'Ticket',
-  'LBL_NOTE_SUBJECT' => 'Oggetto della Nota:',
+  'LBL_NOTE_SUBJECT' => 'Oggetto',
+  'LBL_NOTE_BY' => 'Scritta da',
+  'LBL_NOTE_FOR' => 'Scritta per',
+  'LBL_LIST_DATE_ENTERED' => 'Creata il',
 );
