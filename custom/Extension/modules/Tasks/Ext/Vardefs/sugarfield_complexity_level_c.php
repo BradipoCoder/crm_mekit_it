bfa801667a76148c4fe8d18fa9e11c46149c283c
jakabadambalazs <jakabadambalazs@gmail.com>
2017-11-30 09:00:26 +0100

    committing the uncommitted

---
+++ custom/Extension/modules/Tasks/Ext/Vardefs/sugarfield_complexity_level_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2017-11-06 10:57:36
+$dictionary['Task']['fields']['complexity_level_c']['inline_edit']='1';
+$dictionary['Task']['fields']['complexity_level_c']['labelValue']='Livello di difficoltà';
+
+ ?>
---
+++ custom/Extension/modules/relationships/vardefs/users_cases_1_Users.php
@@ -0,0 +1,12 @@
+<?php
+// created: 2017-11-08 08:27:01
+$dictionary["User"]["fields"]["users_cases_1"] = array (
+  'name' => 'users_cases_1',
+  'type' => 'link',
+  'relationship' => 'users_cases_1',
+  'source' => 'non-db',
+  'module' => 'Cases',
+  'bean_name' => 'Case',
+  'side' => 'right',
+  'vname' => 'LBL_USERS_CASES_1_FROM_CASES_TITLE',
+);
