b51ad9d3d406f91185e2f2fa778d7dac3ffc8799
jakabadambalazs <jakabadambalazs@gmail.com>
2018-03-09 16:03:50 +0100

    Campaign - full html modding

---
+++ custom/modules/Campaigns/language/it_IT.lang.php
@@ -0,0 +1,9 @@
+<?php
+// created: 2016-02-08 14:39:43
+$mod_strings = array (
+  'LBL_TEMPLATE_SAVING' => 'Stai calmo che sto salvando....',
+  'LBL_TEMPLATE_SAVED' => 'Salvato.',
+  'LBL_STEP_INFO_EMAIL_TEMPLATE' => 'Devi selezionare un template esistente o crearne uno nuovo.',
+
+);
+
